273a9494eddac0ce83a08135d5c4cd0d30112269
mnorris3 <mrn3331324@gmail.com>
2025-03-26 12:42:51 -0400

    update

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,26 +1,36 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-
-console.log('DB NAME'+process.env.DB_NAME);
+console.log("DB NAME" + process.env.DB_NAME);
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASSWORD);
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(
+//       process.env.DB_NAME || '',
+//       process.env.DB_USER || '',
+//       process.env.DB_PASSWORD,
+//       {
+//         host: 'localhost',
+//         dialect: 'postgres',
+//         dialectOptions: {
+//           decimalNumbers: true,
+//         },
+//       }
+//     );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for hosted databases
+    },
+  },
+  logging: false,
+});
 
 export default sequelize;
