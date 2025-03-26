fc64bcf5297522b87452f35dc054e86f884158d2
mnorris3 <mrn3331324@gmail.com>
2025-03-26 15:40:43 -0400

    update

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,40 +3,40 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
-// console.log("DB NAME" + process.env.DB_NAME);
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASSWORD);
+console.log("DB NAME: " + process.env.DB_NAME);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
 
-// const sequelize = process.env.DB_URL
-//   ? new Sequelize(process.env.DB_URL)
-//   : new Sequelize(
-//       process.env.DB_NAME || "",
-//       process.env.DB_USER || "",
-//       process.env.DB_PASSWORD,
-//       {
-//         host: "localhost",
-//         dialect: "postgres",
-//         dialectOptions: {
-//           decimalNumbers: true,
-//         },
-//       }
-//     );
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME || "",
+      process.env.DB_USER || "",
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "postgres",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
-const databaseUrl = process.env.DATABASE_URL;
+// const databaseUrl = process.env.DATABASE_URL;
 
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set. Check your environment variables.");
-}
+// if (!databaseUrl) {
+//   throw new Error("DATABASE_URL is not set. Check your environment variables.");
+// }
 
-const sequelize = new Sequelize(databaseUrl, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-  logging: false,
-});
+// const sequelize = new Sequelize(databaseUrl, {
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+//   logging: false,
+// });
 
 export default sequelize;
